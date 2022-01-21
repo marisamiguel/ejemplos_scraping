b9a448d43092c70e0f77e41192c1d56f128b9bd1
lmorillas <morillas@gmail.com>
2022-01-21 09:11:01 +0100

    inicio scraping peliculas

---
+++ lista_peliculas.py
@@ -0,0 +1,25 @@
+import requests
+from lxml import html
+import json
+
+"""
+Tutorial educativo para aprender a analizar y extraer información de páginas web.
+
+La principal tarea es analizar cómo está estructurada la información en la página web.
+Para ello usa el inspector de las herramientas de desarrolladores de tu navegador.
+
+"""
+url = 'https://www.imdb.com/chart/top/'
+res = requests.get(url)
+peliculas = html.fromstring(res.text)
+
+# Extraer las url de las imágenes.
+# Vemos que las imágenes están en un td de clase "posterColumn" dentro de un "a"
+# que tiene un "img" que contiene la url de la película.
+imagenes = peliculas.xpath('//td[contains(@class, "posterColumn")]/a/img/@src')
+
+json.dump(imagenes, open('imagenes.json', 'w'))
+
+# Si queremos analizar todas las películas
+listapelis = peliculas.xpath('//tbody[contains(@class, "lister-list")]/tr')
+
